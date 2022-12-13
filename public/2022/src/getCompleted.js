c58788a02a3b96cf63cbc9095b467c9d81eedd1c
Josa Vicente <jvicenpe@gmail.com>
2022-12-13 22:45:39 +0100

    Day 13: getFilesToBackup

--- public/2022/src/getCompleted.js
+++ public/2022/src/getCompleted.js
@@ -1,12 +1,11 @@
 function getCompleted (part, total) {
-  const toSeconds = (str) => {
-    const [hh, mm, ss] = str.split(':')
-    return (ss * 1) + (mm * 60) + (hh * 3600)
+  const formatTime = (time) => {
+    const [h, m, s] = time.split(':')
+    return (Number(h) * 3600) + (Number(m) * 60) + Number(s)
   }
-
-  const x = toSeconds(part) / toSeconds(total)
-
-  for (let b = 1; b <= toSeconds(total); b++) {
-    if (Number.isInteger(x * b)) return x * b + '/' + b
-  }
+  const gcd = (a, b) => (b) ? gcd(b, a % b) : a
+  const partFormatted = formatTime(part)
+  const totalFormatted = formatTime(total)
+  const gcdEval = gcd(partFormatted, totalFormatted)
+  return `${partFormatted / gcdEval}/${totalFormatted / gcdEval}`
 }
